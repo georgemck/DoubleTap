98f7c261069b3f9743ca23188b340fd5942491f2
GeorgeMcK <george@hookstream.com>
2025-03-07 01:38:19 -0800

    Guided Light

--- DoubleTap/ViewController.swift
+++ DoubleTap/ViewController.swift
@@ -8,10 +8,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var targetView: UIView!
+    @IBOutlet var image01: UIImageView! {
+        image01.isUserInteractionEnabled = true
+        image01.image = UIImage(named: "CreateNewProject.png")
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+//        let doubleTap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(self.doubleTapped(_:)))
+        targetView.isUserInteractionEnabled = true
+        image01.isUserInteractionEnabled = true
+        let doubleTap: UITapGestureRecognizer = UITapGestureRecognizer(target:self,action: Selector( "doubleTapped"))
+        
+        doubleTap.numberOfTapsRequired = 2
+        doubleTap.numberOfTouchesRequired = 2
+        
+        targetView.addGestureRecognizer(doubleTap)
+        
+//        self.image01.addGestureRecognizer(doubleTap)
+//        self.image01.isUserInteractionEnabled = true
+    }
+    
+    @IBAction func doubleTapped(_ sender: Any) {
+        print("double tapped")
+    }
+    @IBAction func touchCallback(recognizer: UITapGestureRecognizer? = nil){
+        if(recognizer?.state == UIGestureRecognizer.State.ended){
+            print("double tapped")
+        }
     }
 
 
